4a7c02c01aaf7777085006cd333d1a2578c7155d
david <70923884+davidildefonso@users.noreply.github.com>
2020-11-20 00:13:41 -0500

    add c#
    add c# script

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+         GetComponent<Rigidbody2D>().isKinematic= true;       
+    }
+
+    void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color =  Color.red;  // new Color(1,0,1);
+    }
+
+    void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color =  Color.white; 
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position =  new Vector3(mousePosition.x,mousePosition.y,transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
